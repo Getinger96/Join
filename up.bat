9998cd0d0c34219807e265509c581f0938ddbe04
Erich Getinger <erich.getinger@outlook.de>
2024-08-09 21:30:44 +0200

    update sideblock

--- up.bat
+++ up.bat
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 git pull
 git add .
-git commit -m "%"
+git commit -m ""
 git push
-=======
-git pull
-git add .
-git commit -m "%*"
-git push
->>>>>>> ea2b0c70747b48158d5b91b431308d32f93528cb
