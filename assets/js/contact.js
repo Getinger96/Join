8eafd65c2bda0da622a1a258b1d806c0b7dbb871
Andre Sutter <andre-sutter@hotmail.com>
2024-09-06 15:40:30 +0200

    add

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -388,64 +388,7 @@ async function putData(path = "", data = {}) {
     return responsASJson = await response.json();
 }
 
-function toggleMenu() {
-    const menu = document.getElementById("contextMenu");
-    menu.style.display = (menu.style.display === "block") ? "none" : "block";
-}
 
-document.addEventListener('click', function(event) {
-    const menu = document.getElementById("contextMenu");
-    const menuButton = document.querySelector('.menu-button-img');
-
-    if (menu && menuButton) {
-        if (!menu.contains(event.target) && !menuButton.contains(event.target)) {
-            menu.style.display = "none";
-        }
-    }
-});
-
-function showMobileNewContactOverlay() {
-    var overlay = document.getElementById("mobileNewContactOverlay");
-    overlay.classList.remove("mobileNewContactOverlay-hidden");
-    overlay.style.display = 'flex'; 
-}
-
-
-function closeMobileNewContact() {
-    var overlay = document.getElementById("mobileNewContactOverlay");
-    overlay.classList.add("mobileNewContactOverlay-hidden");
-    overlay.style.display = 'none'; 
-}
-
-
-function handleAddContactClick(event) {
-    event.stopPropagation();  // Verhindert, dass das übergeordnete div ausgewählt wird
-
-    if (window.innerWidth <= 800) {
-        // Wenn auf einem mobilen Gerät, das mobile Overlay öffnen
-        showMobileNewContactOverlay();
-    } else {
-        addNewContact();  
-    }
-}
-
-
-async function createContactMobile() {
-    const name = document.getElementById('mobileName').value.trim();
-    const email = document.getElementById('mobileMail').value.trim();
-    const phone = document.getElementById('mobilePhone').value.trim();
-
-    const newContact = {
-        name: name,
-        email: email,
-        phone: phone
-    };
-
-    contactsArray.push(newContact);
-    await postData('contacts', newContact);
-    sortContactsByLetter();
-    closeMobileNewContact();
-}
 
 
 
