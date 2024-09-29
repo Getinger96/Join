826dbbb6f22719732b34cf81763d30a9d4979744
Philip Tesch <philiptesch99@gmail.com>
2024-09-29 18:24:40 +0200

    board

--- assets/js/board_database.js
+++ assets/js/board_database.js
@@ -563,8 +563,15 @@ async function createEdittask(index) {
     let status= task.status
     let key = task.taskKey;
     let asignedContacts= task.Assigned;
-   let updatedAssigendContacts= [...asignedContacts, ...assignedContacts];//Die drei Punkte (...), die du in JavaScript gesehen hast, werden als Spread-Operator bezeichnet. Der Spread-Operator wird verwendet, um Elemente eines Arrays, Objekts oder anderen iterierbaren Wertes (wie z.B. Strings oder Sets) zu "entpacken" oder zu "kopieren". Dadurch kannst du auf einfache Weise Arrays kombinieren, Objekte erweitern oder Daten kopieren.
- 
+    let updatedAssigendContacts;
+
+    if (asignedContacts === undefined) {
+        updatedAssigendContacts = [...assignedContacts];
+    }  else {
+
+
+   updatedAssigendContacts= [...asignedContacts, ...assignedContacts];//Die drei Punkte (...), die du in JavaScript gesehen hast, werden als Spread-Operator bezeichnet. Der Spread-Operator wird verwendet, um Elemente eines Arrays, Objekts oder anderen iterierbaren Wertes (wie z.B. Strings oder Sets) zu "entpacken" oder zu "kopieren". Dadurch kannst du auf einfache Weise Arrays kombinieren, Objekte erweitern oder Daten kopieren.
+    }
 
     let editedTASk={
 
