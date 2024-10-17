f2401c001df092b8eb541b28eab90f0bc895dfbb
Erich Getinger <erich.getinger@outlook.de>
2024-10-17 18:44:09 +0200

    overflowhidden

--- assets/js/Desktop_template.js
+++ assets/js/Desktop_template.js
@@ -9,15 +9,23 @@ function setActiveLink() {
     if (currentUrl.includes("summary.html")) {
 
       let summary=document.getElementById("navlinkSummary");
+      let summaryMobile=document.getElementById("navlinkSummarymobil");
+      summaryMobile.classList.add('active');
       summary.classList.add('active');
     } else if (currentUrl.includes("add_task.html")) {
       let addtask=document.getElementById("navlinkaddTask");
+      let addtaskMobil=document.getElementById('navlinkaddTaskmobil')
+      addtaskMobil.classList.add("active");
       addtask.classList.add("active");
     } else if (currentUrl.includes("board.html")) {
+      let boardMobile=document.getElementById('navlinkBoardMobile');
       let board=document.getElementById("linkBoard");
+      boardMobile.classList.add("active");
       board.classList.add("active");
     } else if (currentUrl.includes("contact.html")) {
        let contact= document.getElementById("navlinkContact");
+       let contactMobile=documnet.getElementById('navlinkContactMobile')
+       contactMobile.classList.add("active");
        contact.classList.add("active");
       }
 
